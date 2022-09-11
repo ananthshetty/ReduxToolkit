bebd9242d755ee778c35e2cc0ce41415789cc512
ananthshetty <107457713+ananthshetty@users.noreply.github.com>
2022-09-11 14:40:17 +0530

    finalCommit

---
+++ src/store/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Loading() {
+  return (
+    <div style={{display:'flex',justifyContent:'center',alignItems:'center',height:'30vh',backgroundColor:'#0000000a'}}>
+        <div class="spinner-border" role="status">
+        </div>
+    </div>
+  )
+}
