9c6614a658544438f18f395048c921aec8308f02
ananthshetty <107457713+ananthshetty@users.noreply.github.com>
2022-09-11 14:56:30 +0530

    Update Loading.js

--- src/store/Loading.js
+++ src/store/Loading.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Loading() {
   return (
     <div style={{display:'flex',justifyContent:'center',alignItems:'center',height:'30vh',backgroundColor:'#0000000a'}}>
-        <div class="spinner-border" role="status">
+        <div className="spinner-border" role="status">
         </div>
     </div>
   )
